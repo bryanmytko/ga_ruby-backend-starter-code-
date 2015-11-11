7fea1a1b4be73bc79776890ae07989eaffd5bfd8
Bryan Mytko <bryanmytko@gmail.com>
2015-11-11 15:55:03 -0500

    Add favoriting logic

--- app.rb
+++ app.rb
@@ -19,22 +19,37 @@ get "/search.json" do
     response = client.search_by_title(title)
   elsif keyword = params[:keyword]
     response = client.search_by_keyword(keyword)
+
+    response["Search"].each do |r|
+      if Favorite.find_by(imdb_id: r["imdbID"], user_id: params[:user_id])
+        r["Favorite"] = true;
+      end
+    end unless response["Error"]
   end
 
   response.to_json
 end
 
 get "/favorites.json" do
+  client = Sinatra::Omdb::Client.new
   response.header["Content-Type"] = "application/json"
-  File.read("data.json")
+  response = []
+
+  favorites = Favorite.where(user_id: params[:user_id])
+  favorites.each do |favorite|
+    data = client.search_by_id(favorite.imdb_id)
+    data["Favorite"] = true
+    response << data
+  end
+
+  response.to_json
 end
 
-patch "/favorites" do
-  byebug
-  file = JSON.parse(File.read("data.json"))
-  return "Invalid Request" unless params[:name] && params[:oid]
-  movie = { name: params[:name], oid: params[:oid] }
-  file << movie
-  File.write("data.json", JSON.pretty_generate(file))
-  movie.to_json
+post "/favorites" do
+  Favorite.create(
+    user_id: params[:user_id],
+    imdb_id: params[:imdb_id]
+  )
 end
+
+class Favorite < ActiveRecord::Base; end
